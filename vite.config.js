b6e759a48b7b092a60c4ef3702859c5e76057034
Alicja Bieryt <alicjabieryt@gmail.com>
2024-11-15 16:51:53 +0100

    github pages configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Arteterapia-Client",
   plugins: [react()],
   resolve: {
     mainFields: [],
