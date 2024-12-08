fcb77a994712101cb7673e7f528d1aad7bc98e7f
Alicja Bieryt <alicjabieryt@gmail.com>
2024-12-08 23:32:37 +0100

    image slider finished

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,53 +1,43 @@
-import { useState } from "react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const ImageSlider = () => {
-  const [startIndex, setStartIndex] = useState(0);
-  const visibleCount = 4;
-
   const images = [
+    "didacticsPhoto1.jpg",
+    "didacticsPhoto2.jpg",
+    "didacticsPhoto3.jpg",
+    "didacticsPhoto4.jpg",
     "didacticsPhoto1.jpg",
     "didacticsPhoto2.jpg",
     "didacticsPhoto3.jpg",
     "didacticsPhoto4.jpg",
   ];
 
-  const visibleImages = images
-    .slice(startIndex, startIndex + visibleCount)
-    .concat(
-      images.slice(0, Math.max(0, startIndex + visibleCount - images.length))
-    );
-
-  const handleNext = () => {
-    setStartIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const handlePrev = () => {
-    setStartIndex(
-      (prevIndex) => (prevIndex - 1 + images.length) % images.length
-    );
-  };
-
   return (
-    <div className="gallery flex">
-      <button onClick={handlePrev} className="controls" id="left">
-        <span>&lt;</span>
-      </button>
-      {visibleImages.map((src, index) => (
-        <img
-          key={index}
-          src={src}
-          alt={`Gallery item ${index}`}
-          className="gallery-image"
-        />
+    <Swiper
+      modules={[Pagination, Autoplay, Navigation]}
+      pagination={{ clickable: true }}
+      navigation={true}
+      spaceBetween={50}
+      slidesPerView={4}
+      loop={true}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+      }}
+      speed={1000}
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <div className="slideContainer">
+            <img src={image} alt="slider photo" />
+          </div>
+        </SwiperSlide>
       ))}
-      <button onClick={handleNext} className="controls" id="right">
-        <span>&gt;</span>
-      </button>
-      {/* <img src="didacticsPhoto1.jpg" alt="" />
-      <img src="didacticsPhoto2.jpg" alt="" />
-      <img src="didacticsPhoto3.jpg" alt="" />
-      <img src="didacticsPhoto4.jpg" alt="" /> */}
-    </div>
+    </Swiper>
   );
 };
 
