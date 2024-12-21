1e13f6552269cc03cd069897f263cf2044be256e
Alicja Bieryt <alicjabieryt@gmail.com>
2024-12-21 17:42:53 +0100

    code cleanup

--- src/config.js
+++ src/config.js
@@ -19,9 +19,6 @@ export const artCategories = [
   },
 ];
 
-export const TOKEN =
-  "eyJhbGciOiJQUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlN0aXhFbmpveWVyIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.K_gzNuovEaB91Y2y5vTnyi8A5TKxLWOQtgglu6aMdo1Npw2nC--aiyUe_yde3CeXeaB5Wfy710VAGrUKuVVRUcQ8DoXqz-7FMKAdd-Adwd10lGTIkXRP6Ab5dTSd9BAtK0dYQE4lVSWIekYuy1bWZATmyepdrqNeHAq4khVXWtCAUJyANu8MTuppn4D3hQD8heo_4r4SdPGgFficoLsqYpjqqySAghayOpjAt76b399PNCdwSR2DeoIHsW234sN_yviyG0hl5Q9xWqPzLWEmVywCwuHVEB-zazNY_L3kL9W2fBHVwiePgXIXXq3bcBV_c3iYVYie-XXgYpVpbOuzyg";
-
 // const BASE_URL = {
 //     api: import.meta.env.VITE_STRAPI_URL
 // }
